6d672cd798988b8c7f4878eebd524afef7c19efd
joemarlo <joe.marlo.09@gmail.com>
2020-08-16 19:48:19 -0400

    cleaned up comments

--- Scraping_WSB/Scrape_WSB.py
+++ Scraping_WSB/Scrape_WSB.py
@@ -17,9 +17,8 @@ user_agent =  "Get_Stonks by /u/tall_george_")
 # make sure we're in read-only mode
 reddit.read_only = True
 
-# use PRAW credentials; then PSAW returns the IDs that you can use in PRAW
+# use PRAW credentials; then PSAW returns the IDs that we can use in PRAW
 api = PushshiftAPI(reddit)
-
 
 # set range of dates to scrape
 start_day = dt.datetime(2020, 1, 1)
@@ -28,6 +27,7 @@ date_list = [start_day + dt.timedelta(days=x) for x in range(50)]
 # create empty list to hold submission ids
 all_ids = list()
 
+# iterate through the dates and pull the posts
 for day in date_list:
     # set starting day for this loop
     start_epoch=int(day.timestamp())
@@ -47,13 +47,12 @@ for day in date_list:
 # flatten list
 all_ids = [item for sublist in all_ids for item in sublist]
 
-# get flairs associated the results id
+# get flairs associated with the results id
 flairs = list()
 for submission in all_ids:
     flairs.append(submission.link_flair_text)
 
 # get submission ids that match "DD" (daily discussion)
-
 DD_ids = list(np.array(all_ids)[np.array(flairs) == "DD"])
 
 # define dict of the items we want to pull
@@ -81,16 +80,16 @@ for submission in DD_ids:
 # convert dict to dataframe
 items_df = pd.DataFrame(items_dict)
 
+# define functino to get the date form the timestamp
 def get_date(created):
     return dt.datetime.fromtimestamp(created)
 
-# clean up date
+# clean up date column
 items_df['date'] = items_df["created"].apply(get_date)
 
 # remove rows that contained removed posts, deleted
 items_df = items_df[items_df['body'] != '[removed]']
 items_df = items_df[items_df['body'] != '[deleted]']
-
 
 # write out dataframe
 items_df.to_csv("scraped_posts.csv", index=False)
